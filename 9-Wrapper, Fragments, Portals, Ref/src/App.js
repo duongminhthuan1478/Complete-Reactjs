698d88e83d10aecd96e02c69f959d73d2d24425a
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2022-10-30 19:27:20 +0700

    JSX limitation nested div, using React.Fragment to skip redundant div

--- 9-Wrapper, Fragments, Portals, Ref/src/App.js
+++ 9-Wrapper, Fragments, Portals, Ref/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +16,11 @@ function App() {
   };
 
   return (
-    <div>
+    // Using <React.Fragment> </React.Fragment> or <> </> to skip this redundant
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 }
 
