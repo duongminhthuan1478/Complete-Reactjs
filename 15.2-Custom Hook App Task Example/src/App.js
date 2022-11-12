074039883a66f680a6f9654df887ef25cc9ba65a
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2022-11-12 19:17:07 +0700

    Custom UseHttp hook for reusable http request post and get,...

--- 15.2-Custom Hook App Task Example/src/App.js
+++ 15.2-Custom Hook App Task Example/src/App.js
@@ -1,43 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const url = "https://react-http-7aa09-default-rtdb.firebaseio.com/tasks.json";
   const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformData = (res) => {
+      const tasks = [];
+      for (const taskKey in res) {
+        tasks.push({ id: taskKey, text: res[taskKey].text });
+      }
+      setTasks(tasks);
+    };
+
+    fetchTasks({ url }, transformData);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- 15.2-Custom Hook App Task Example/src/components/NewTask/NewTask.js
+++ 15.2-Custom Hook App Task Example/src/components/NewTask/NewTask.js
@@ -1,41 +1,32 @@
-import { useState } from 'react';
+import useHttp from "../../hooks/use-http";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const url = "https://react-http-7aa09-default-rtdb.firebaseio.com/tasks.json";
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, res) => {
+    console.log("createTask", taskText, res);
+    const generatedId = res.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url,
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      // khi sử dụng bind, tham số res sẽ được tự động thêm vào tham số res khi useHttp đã sử dụng applyDataFn(data); vì vậy res sẽ tự động có data
+      createTask.bind(this, taskText)
+    );
   };
 
   return (
