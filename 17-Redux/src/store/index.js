cd0eedf5df49217bf86d07601bc623c79510687f
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2022-11-16 20:36:21 +0700

    React redux

---
+++ 17-Redux/src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore } from "redux";
+
+const initState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initState, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1, showCounter: state.showCounter };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  }
+
+  if (action.type === "toggle") {
+    return { counter: state.counter, showCounter: !state.showCounter };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
